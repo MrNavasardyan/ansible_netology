d7464ea892222e3a3d05d5543a45a0a6d60c3d7b
ngrachik <ngrachik2011@gmail.com>
2022-07-15 22:22:08 +0300

    modified:   08-ansible-01-base/playbook/script.sh

--- 08-ansible-01-base/playbook/script.sh
+++ 08-ansible-01-base/playbook/script.sh
@@ -2,25 +2,25 @@
 
 set -e
 
-#deb=$(docker images | grep bitnami | awk '{print $1}')
-#echo $deb && docker run -dti --name debian $deb
+deb=$(docker images | grep bitnami | awk '{print $1}')
+echo $deb && docker run -dti --name debian $deb
 
-#cent=$(docker images | grep "centos" | awk '{print $1}')
-#echo $cent && docker run -dti --name centos $cent
+cent=$(docker images | grep "centos" | awk '{print $1}')
+echo $cent && docker run -dti --name centos $cent
 
-#fed=$(docker images | grep fedora | awk '{print $1}')
-#echo $fed && docker run -dti --name fedora $fed
+fed=$(docker images | grep fedora | awk '{print $1}')
+echo $fed && docker run -dti --name fedora $fed
 
-cd ./docker
+# cd ./docker
 
-docker compose up -d
+# docker compose up -d
 
 docker ps
 
-cd ..
+# cd ..
 
 ansible-playbook -i inventory/prod.yml site.yml
 
-docker compose down
+# docker compose down
 
-# docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
+docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
