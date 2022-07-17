5b3b6409c37d3be8c819e2b280647358a8f8bfe3
ngrachik <ngrachik2011@gmail.com>
2022-07-17 22:10:43 +0300

    Changes to be committed: 	modified:   book_ansible/django_postgres/dockerscript.sh

--- book_ansible/django_postgres/dockerscript.sh
+++ book_ansible/django_postgres/dockerscript.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-ubu=$(docker images | grep 27941809078c | awk '{print $1}')
+ubu=$(docker images | grep 07c9ba902d20 | awk '{print $1}')
 echo $deb && docker run -dti -p 8881:80 -p 8443:443 --name ubuntu1 $ubu
 
-ubu=$(docker images | grep 27941809078c | awk '{print $1}')
+ubu=$(docker images | grep 07c9ba902d20 | awk '{print $1}')
 echo $deb && docker run -dti -p 8882:80 -p 8444:443 --name ubuntu2 $ubu
 
-ubu=$(docker images | grep 27941809078c | awk '{print $1}')
+ubu=$(docker images | grep 07c9ba902d20 | awk '{print $1}')
 echo $deb && docker run -dti -p 8883:80 -p 8445:443 --name ubuntu3 $ubu
 
 docker ps
